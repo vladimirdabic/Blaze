491806003fbb4822eaab599a5d3e468c7cb3fa10
Владимир Дабић <dabkegames@gmail.com>
2023-11-12 23:05:24 +0100

    Fix the newly added core project

---
+++ Blaze Core/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blaze_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args) { }
+    }
+}
